8927f6a01a1837e1944d8ff154e35bbe9faf3f6b
Lotharie <49253492+LotharieSlayer@users.noreply.github.com>
2021-11-28 15:04:39 +0100

    1.0

---
+++ src/files/modules.js
@@ -0,0 +1,11 @@
+/**
+ * @author Lothaire Guée
+ * @description
+ * 		The file contains authorisation for module to execute.
+ */
+
+/**
+ * Return if yes or no the module is authorize to execute
+ * @export {boolean} Authorisation.
+ */
+exports.Example = true;
